3253e70a7d639d69108d666aedb0bf5faf0b5082
Ali Servet Donmez <asd@pittle.org>
2013-10-19 08:17:23 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forx::Application.config.secret_key_base = '5002d58631c3707abcca6f2c716f2b50c2987c0a8c62e97fad98bff97a66a6bf621e0f6648676fe80907df81e00338154982a3090075bc78c9d997569f6d6dc0'
