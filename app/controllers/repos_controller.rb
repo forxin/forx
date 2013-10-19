5037ca612a8337d857f9499d5fe4947cca3ef947
Antonio Scandurra <as_cii@hotmail.it>
2013-10-19 14:01:05 +0200

    Improve fake data (more random results)

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,7 +1,11 @@
 class ReposController < ApplicationController
   def index
-    @repositories = 10.times.map do |i|
-      Repository.new("Repo #{i}", "http://www.example.com", "https://github.com/images/error/octocat_happy.gif", "octocat", 10)
+    hub_repos = 10.times.map do |i|
+      GithubRepo.new("Repo #{i}", "http://www.example.com", "https://github.com/images/error/octocat_happy.gif",
+        "octocat", rand(0..400), rand(60.days.ago.to_date..Date.today),
+        rand(60.days.ago.to_date..Date.today))
     end
+
+    @repositories = Ranker.rate_repos(*hub_repos)
   end
 end
