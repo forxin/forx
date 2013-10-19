28305b37d14d589d12fe2dddd4e04c09a9d7c297
Kseniia <kseniia.pelykh@gmail.com>
2013-10-19 14:01:56 +0200

    some changes

--- app/assets/javascripts/draw.js
+++ app/assets/javascripts/draw.js
@@ -1,27 +1,30 @@
 function draw(count)
 {
-	var centerX = 500;
-	var centerY = 200;
-	var maxDistanse = 200; 
+	var centerX;
+	var centerY;
+	var maxDistanse; 
 	var u;
 	var a;
 	var radius = new Array();
 	var x = new Array();
 	var y = new Array();
 	
-	/*
 	for (i=0; i < count; i++) {
 		radius[i] = rating[i] + 100;
-		}
+	}
 	
-	maxDistanse = Math.max.apply(Math, radius) + 50;
+	maxDistanse = Math.max.apply(Math, radius) + radius[0] + 10;
+	
+	centerX = $(window).width() / 2;
+	centerY = $(window).scrollTop() + $(window).height() / 2;
+
+	x[0] = certerX;
+	y[0] = centerY;
 		
-		*/
-	
 	u = Math.PI;
 	a = Math.PI * 2 / count;
 	
-	for (i=0; i< count; i++)
+	for (i=1; i<= count; i++)
 	{
 		x[i] = centerX+maxDistanse*Math.cos(u);
 		y[i] = centerY+maxDistanse*Math.sin(u);
@@ -29,7 +32,7 @@ function draw(count)
 	}
 	
 	$('.repository').each(function( index ) { 
-	($(this).css( {"position":"absolute","left":x[index],"top":y[index],"background-color":"cadetblue","width":"100px","height":"100px"}))//color, width, height need to change
+	($(this).css( {"position":"absolute","left":x[index],"top":y[index],"background-color":"cadetblue","width":radius[i]*2,"height":radius[i]*2}))//color need to change
 	});
 }
 
